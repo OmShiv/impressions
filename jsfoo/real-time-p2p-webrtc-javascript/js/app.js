81100e3dd50d92e249793e2ef3d399a46d54a456
Om Shankar <invincible.om@gmail.com>
2013-09-14 12:59:21 +0530

    I need to learn git

---
+++ jsfoo/real-time-p2p-webrtc-javascript/js/app.js
@@ -0,0 +1,51 @@
+// Enable State changer
+
+var SlideShowApp = (function(doc, win) {
+
+    var stateArray = [
+            'default',
+            'alert',
+            'orange',
+            'soothe',
+            'blackout',
+            'whiteout',
+            'cobalt',
+            'mint',
+            'submerge',
+            'lila',
+            'sunset'
+        ],
+        stateCounter = 0;
+
+    function stateChanger() {
+        doc.querySelector('#state-changer').addEventListener('click', function(){
+            stateCounter = stateCounter == 9 ? 0 : stateCounter;
+            this.innerText = (doc.documentElement.className = stateArray[stateCounter++]);
+        });
+    }
+
+    function hoverEffectFirstSlide() {
+        Reveal.addEventListener( 'ready', function( event ) {
+            var domBase = event.currentSlide.querySelector('#hero-logo');
+                childImages = Array.prototype.slice.call(domBase.children),
+                effectCounter = 0;
+            var effectInterval = win.setInterval(function(){
+                effectCounter = effectCounter == 3 ? 0 : effectCounter;
+                childImages.forEach(function(el){
+                    el.className = '';
+                });
+                childImages[effectCounter++].className = 'hover-effect';
+            }, 1000);
+        });
+    }
+
+    return {
+        init: function() {
+            stateChanger();
+            hoverEffectFirstSlide();
+        }
+    }
+}(window.document, window));
+
+SlideShowApp.init();
+
